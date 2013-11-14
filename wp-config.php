26b7dec9ee7ba631990ad2512a9918aec265a29c
Hugo Espinosa <vaporic@gmail.com>
2013-11-14 17:28:28 -0600

    test

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
 define('DB_NAME', 'interlingua_sitio');
 
 /** MySQL database username */
-define('DB_USER', '');
+define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'huo0lpaw');
