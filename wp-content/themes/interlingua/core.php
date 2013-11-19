4e2268c239d15f8d78376ce5d8db6917b80dbc9d
Hugo Espinosa <vaporic@gmail.com>
2013-11-19 13:03:48 -0600

    add

--- wp-content/themes/interlingua/core.php
+++ wp-content/themes/interlingua/core.php
@@ -231,7 +231,11 @@ function stripAccents($cadena){
     return utf8_encode($cadena);
 }
 
-function saveMagazine(){		
+function saveMagazine(){	
+	error_reporting(E_ERROR);
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);	
+	
 	//Global Values
 	$matricula = strtoupper($_POST["matricula"]);
 	$paterno = ucfirst(stripAccents($_POST["paterno"]));
