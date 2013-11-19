cfe7b578a1b1ec3c7905189971c4af0c06f8c6b0
Hugo Espinosa <vaporic@gmail.com>
2013-11-19 15:18:09 -0600

    add

--- wp-content/themes/interlingua/core.php
+++ wp-content/themes/interlingua/core.php
@@ -231,8 +231,7 @@ function stripAccents($cadena){
     return utf8_encode($cadena);
 }
 
-function saveMagazine(){
-	echo "string";	
+function saveMagazin(){	
 	//error_reporting(E_ERROR);
 	//error_reporting(E_ALL);
 	//ini_set("display_errors", 1);	
