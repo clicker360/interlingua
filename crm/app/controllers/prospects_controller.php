54d7c0422b8c787aab78b1eeaddb210f4da1fea0
Hugo Espinosa <vaporic@gmail.com>
2013-12-05 12:54:56 -0600

    add

--- crm/app/controllers/prospects_controller.php
+++ crm/app/controllers/prospects_controller.php
@@ -910,6 +910,7 @@ class ProspectsController extends AppController{
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST');  
         $this->autoRender = false;
+        Configure::write('debug', '2');
 
         // Variables de configuración
         $response    = array();
