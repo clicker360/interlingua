05c8a87b3d260ec4fafad7675da6a2a1d2256a41
Hugo <hugo@develop.cc>
2014-01-23 09:28:23 -0600

    add

--- wp-content/themes/interlingua/header.php
+++ wp-content/themes/interlingua/header.php
@@ -210,7 +210,7 @@ _gaq.push(['_trackPageview']);
 	window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 	d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 	_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-	$.src='//cdn.zopim.com/?1g1vE7v5adkEtsV6o9kwO8Ec2cp8OIQF';z.t=+new Date;$.
+	$.src='//v2.zopim.com/?1qdTKWEfbdERJo2vkOeZvkwDidMTesBF';z.t=+new Date;$.
 	type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 	</script>
 	<!--End of Zopim Live Chat Script-->
