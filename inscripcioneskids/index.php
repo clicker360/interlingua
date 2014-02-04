1c099e2f2b948a3a1f00788bf58261298dc1c760
Hugo <hugo@develop.cc>
2014-02-04 10:16:21 -0600

    add

--- inscripcioneskids/index.php
+++ inscripcioneskids/index.php
@@ -166,5 +166,24 @@ valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
   </div>
 </footer>
 
+	<script type="text/javascript">
+	function setCookie(name, value, days){
+	    var date = new Date();
+	    date.setTime(date.getTime() + (days*24*60*60*1000)); 
+	    var expires = "; expires=" + date.toGMTString();
+	    document.cookie = name + "=" + value + expires;
+	}
+	function getParam(p){
+	    var match = RegExp('[?&]' + p + '=([^&]*)').exec(window.location.search);
+	    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+	}
+	var gclid = getParam('gclid');
+	if(gclid){
+	    var gclsrc = getParam('gclsrc');
+	    if(!gclsrc || gclsrc.indexOf('aw') !== -1){
+		    setCookie('gclid', gclid, 90);
+		}
+	}
+	</script>
 </body>
 </html>
