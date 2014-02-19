fc84bc6a8bd9e0d3b5aab8fb417b4bff1295e3bd
Hugo <hugo@localhost.localdomain>
2014-02-19 13:55:38 -0600

    add

--- wp-content/themes/interlingua/page-alumnos.php
+++ wp-content/themes/interlingua/page-alumnos.php
@@ -188,52 +188,74 @@ Template Name: Alumnos
 						//         echo $archivo . "<br />";
 						//     }
 						// } 
-  						function leer_archivos_y_directorios($ruta){
-						    if (is_dir($ruta))
-						    {
-						        // Abrimos el directorio y comprobamos que
-						        if ($aux = opendir($ruta))
-						        {
-						            while (($archivo = readdir($aux)) !== false)
-						            {
-						                // Si quisieramos mostrar todo el contenido del directorio pondríamos lo siguiente:
-						                // echo '<br />' . $file . '<br />';
-						                // Pero como lo que queremos es mostrar todos los archivos excepto "." y ".."
-						                if ($archivo!="." && $archivo!="..")
-						                {
-						                    $ruta_completa = $ruta . '/' . $archivo;
+  				// 		function leer_archivos_y_directorios($ruta){
+						//     if (is_dir($ruta))
+						//     {
+						//         // Abrimos el directorio y comprobamos que
+						//         if ($aux = opendir($ruta))
+						//         {
+						//             while (($archivo = readdir($aux)) !== false)
+						//             {						                
+						//                 if ($archivo!="." && $archivo!="..")
+						//                 {
+						//                     $ruta_completa = $ruta . '/' . $archivo;						 						                    
+						//                     if (is_dir($ruta_completa))
+						//                     {
+						//                         echo "<br /><strong>Directorio:</strong> " . $ruta_completa;
+						//                         leer_archivos_y_directorios($ruta_completa . "/");
+						//                     }
+						//                     else
+						//                     {
+						//                         echo '<br />' . $archivo . '<br />';
+						//                     }
+						//                 }
+						//             }
 						 
-						                    // Comprobamos si la ruta más file es un directorio (es decir, que file es
-						                    // un directorio), y si lo es, decimos que es un directorio y volvemos a
-						                    // llamar a la función de manera recursiva.
-						                    if (is_dir($ruta_completa))
-						                    {
-						                        echo "<br /><strong>Directorio:</strong> " . $ruta_completa;
-						                        leer_archivos_y_directorios($ruta_completa . "/");
-						                    }
-						                    else
-						                    {
-						                        echo '<br />' . $archivo . '<br />';
-						                    }
-						                }
-						            }
+						//             closedir($aux);
 						 
-						            closedir($aux);
-						 
-						            // Tiene que ser ruta y no ruta_completa por la recursividad
-						            echo "<strong>Fin Directorio:</strong>" . $ruta . "<br /><br />";
-						        }
-						    }
-						    else
-						    {
-						        echo $ruta;
-						        echo "<br />No es ruta valida";
-						    }
-						}
+						//             // Tiene que ser ruta y no ruta_completa por la recursividad
+						//             echo "<strong>Fin Directorio:</strong>" . $ruta . "<br /><br />";
+						//         }
+						//     }
+						//     else
+						//     {
+						//         echo $ruta;
+						//         echo "<br />No es ruta valida";
+						//     }
+						// }
 
-						$ruta = "/var/www/InterlinguaAudios/";
-					   	leer_archivos_y_directorios($ruta);
+						// $ruta = "/var/www/InterlinguaAudios/";
+					 //   	leer_archivos_y_directorios($ruta);
 					?>
+
+					<div class="wpfilebase-file-default" onclick="if('undefined' == typeof event.target.href) document.getElementById('wpfb-file-link-%uid%').click();">
+					  <div class="icon"><a href="%file_url%" target="_blank" title="Download %file_display_name%"><img align="middle" src="%file_icon_url%" alt="%file_display_name%" /></a></div>
+					  <div class="filetitle">
+					    <a href="%file_url%" title="Download %file_display_name%" target="_blank" id="wpfb-file-link-%uid%">%file_display_name%</a>
+					    <!-- IF %file_post_id% AND %post_id% != %file_post_id% --><a href="%file_post_url%" class="postlink">» %'Post'%</a><!-- ENDIF -->
+					    <br />
+					    %file_name%<br />
+					    <!-- IF %file_version% -->%'Version:'% %file_version%<br /><!-- ENDIF -->
+					  </div>
+					  <div class="info">
+					    %file_size%<br />
+					    %file_hits% %'Downloads'%<br />
+					    <a href="#" onclick="return wpfilebase_filedetails(%uid%);">%'Details'%</a>
+					  </div>
+					  <div class="details" id="wpfilebase-filedetails%uid%" style="display: none;">
+					  <!-- IF %file_description% --><p>%file_description%</p><!-- ENDIF -->
+					  <table border="0">
+					   <!-- IF %file_languages% --><tr><td><strong>%'Languages'%:</strong></td><td>%file_languages%</td></tr><!-- ENDIF -->
+					   <!-- IF %file_author% --><tr><td><strong>%'Author'%:</strong></td><td>%file_author%</td></tr><!-- ENDIF -->
+					   <!-- IF %file_platforms% --><tr><td><strong>%'Platforms'%:</strong></td><td>%file_platforms%</td></tr><!-- ENDIF -->
+					   <!-- IF %file_requirements% --><tr><td><strong>%'Requirements'%:</strong></td><td>%file_requirements%</td></tr><!-- ENDIF -->
+					   <!-- IF %file_category% --><tr><td><strong>%'Category:'%</strong></td><td>%file_category%</td></tr><!-- ENDIF -->
+					   <!-- IF %file_license% --><tr><td><strong>%'License'%:</strong></td><td>%file_license%</td></tr><!-- ENDIF -->
+					   <tr><td><strong>%'Date'%:</strong></td><td>%file_date%</td></tr>
+					  </table>
+					  </div>
+					 <div style="clear: both;"></div>
+					</div>
 
 
             		<table style="width:100%;border:solid 2px #33B0BE;">
